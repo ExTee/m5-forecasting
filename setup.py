f54f172a837f2166f21151027094ead1fc1681dc
ExTee <xintong.wang1995@gmail.com>
2020-03-09 19:18:55 -0400

    [NEW] cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Estimate the unit sales of Walmart retail goods',
+    author='ExTea',
+    license='',
+)
